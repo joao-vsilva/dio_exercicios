53ac4465e7145cc9354474028e990f38c34e54af
joao-vsilva <166138766+joao-vsilva@users.noreply.github.com>
2024-04-19 15:33:49 -0300

    update_and_rename_banco_v0.1.py to banco_v0.2.py
    Eliminação do bag de "dinheiro infinito"

--- banco/banco_principal/banco_v0.2.py
+++ banco/banco_principal/banco_v0.2.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-resposta = int(input("""
+menu = """
       [1] para depositar
       [2] para fazer um saque
       [3] para ver o extrato
       [0] para sair
-      : """))
+      : """
+
+resposta = int(input(menu))
 
 limite_saque_diario = 3
 MAX_VALOR_SAQUE = 500
@@ -27,12 +29,7 @@ while True:
             extrato_deposito[contador] = f"{ordem2}º depósito R$ {deposito:.2f}"
             contador += 1
             conta += deposito
-            resposta = int(input("""
-      [1] para depositar
-      [2] para fazer um saque
-      [3] para ver o extrato
-      [0] para sair
-      : """))
+            resposta = int(input(menu))
             
     elif resposta == 2:
         if limite_saque_diario > 0 and limite_saque_diario <= 3:
@@ -40,32 +37,18 @@ while True:
             if valor_saque > 0 and valor_saque <= 500:
                 if valor_saque <= conta:
                     print("O dinheiro está saindo na boca do caixa...")
+                    conta -= valor_saque
                     limite_saque_diario -= 1
                     ordem = posicao + 1
                     extrato_saque[posicao] = f"{ordem}º saque R$ {valor_saque:.2f}"
                     posicao += 1
-                    resposta = int(input("""
-      [1] para depositar
-      [2] para fazer um saque
-      [3] para ver o extrato
-      [0] para sair
-      : """))
+                    resposta = int(input(menu))
                 else:
                     print("Saldo insuficiente para a operação\nSelecione uma das opções abaixo")
-                    resposta = int(input("""
-      [1] para depositar
-      [2] para fazer um saque
-      [3] para ver o extrato
-      [0] para sair
-      : """))
+                    resposta = int(input(menu))
             else:# Melhorar depois
                 print("Cada saque é realizado com até R$ 500,00\n Selicione uma das opções abaixo")
-                resposta = int(input("""
-      [1] para depositar
-      [2] para fazer um saque
-      [3] para ver o extrato
-      [0] para sair
-      : """))
+                resposta = int(input(menu))
         else:
             print("Você já alcançou o seu limite máximo de saques.\nVolte em outro dia útil")
             resposta = int(input("""
@@ -75,13 +58,9 @@ while True:
       : """))
                 
     elif resposta == 3: # Melhorar depois
+        print(f"Você possui R${conta:.2f}")
         for i in range(3):
             print(extrato_saque[i])
         for i in range(contador):
             print(extrato_deposito[i])
-        resposta = int(input("""
-      [1] para depositar
-      [2] para fazer um saque
-      [3] para ver o extrato
-      [0] para sair
-      : """))
+        resposta = int(input(menu))
